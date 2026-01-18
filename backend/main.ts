6b5959d16786b449f775914146f327d53a083c4f
igor balbino gluck <igbalbinnogluck@gmail.com>
2026-01-18 12:36:15 -0300

    funcao enviar email e criacao do webhook

--- backend/main.ts
+++ backend/main.ts
@@ -1,6 +1,41 @@
 import cors from 'cors';
-import express from 'express';
-import { MercadoPagoConfig, Preference } from 'mercadopago';
+import express, { query } from 'express';
+import nodemailer from 'nodemailer';
+import { MercadoPagoConfig, Preference, Payment } from 'mercadopago';
+
+// configuração do transportador 
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'email@gmail.com', //  e-mail
+    pass: 'xxxx xxxx xxxx xxxx'  
+  }
+});
+
+// função para disparar o e-mail
+async function enviarProduto(emailCliente: string) {
+  try {
+    const mailOptions = {
+      from: '"Vendas Blox Fruit" <seu-email@gmail.com>',
+      to: emailCliente,
+      subject: 'Seu curso chegou! Aproveite o acesso vitalício',
+      html: `
+        <div style="font-family: sans-serif; line-height: 1.6;">
+          <h2>Obrigado pela sua compra!</h2>
+          <p>O pagamento do seu <b>Curso Blox Fruit</b> foi confirmado.</p>
+          <p>Você pode acessar seu PDF clicando no botão abaixo:</p>
+          <a href="LINK_DO_SEU_PDF_AQUI" style="display: inline-block; padding: 12px 25px; background-color: #28a745; color: white; text-decoration: none; border-radius: 5px;">Baixar PDF Agora</a>
+        </div>
+      `
+    };
+
+    await transporter.sendMail(mailOptions);
+    console.log(` Email enviado para: ${emailCliente}`);
+  } catch (error) {
+    console.error('❌ Erro ao enviar e-mail:', error);
+  }
+}
+
 
 
 const app = express();
@@ -50,3 +85,43 @@ const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Servidor rodando em http://localhost:${PORT}`);
 });
+
+
+
+//WEBHOOK pega o status e id do pagamento confere e dispara o email
+
+
+app.post('/webhook', async (req, res) => {
+  const { query } = req;
+
+
+  if(query.type == 'payment'){
+    const paymentId = String(query['data.id']);
+    try {
+
+      //consultar para saber o status real
+      const payment = new Payment(client);
+      const paymentDetails = await payment.get({ id: paymentId });
+
+      const status = paymentDetails.status;
+      const emailCliente = paymentDetails.payer?.email;
+      console.log(`Pagamento ${paymentId} está ${status}`);
+
+      // payment = aprovado => dispara o email
+      if (status === 'approved' && emailCliente) {
+        await enviarProduto(emailCliente);
+      }
+
+    } catch (error) {
+      console.error('Erro ao buscar pagamento:', error);
+    }
+  }
+
+  res.sendStatus(200);
+});
+  
+ 
+
+
+
+
